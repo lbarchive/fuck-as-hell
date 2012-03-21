afa8a34a9cd2c60b29b9957f5b96fcc9c789d41f
Yu-Jie Lin <livibetter@gmail.com>
2012-03-21 21:11:52 +0800

    Add License text

--- fash.c
+++ fash.c
@@ -1,3 +1,13 @@
+// Deletemiddle )ↄ( 157+163+167+173+179+181+191+193+197+199+212 pie yesbuddy
+//
+// (* {- =begin <!--- <!--
+// ! C REM ' ; -- " # /* This program is free software. It comes without any warranty, to
+// ! C REM ' ; -- " #  * the extent permitted by applicable law. You can redistribute it
+// ! C REM ' ; -- " #  * and/or modify it under the terms of the Do What The Fuck You Want
+// ! C REM ' ; -- " #  * To Public License, Version 2, as published by Sam Hocevar. See
+// ! C REM ' ; -- " #  * http://sam.zoy.org/wtfpl/COPYING for more details. */
+// *) -} =end   --->  -->
+
 int
 main (int argc, char **argv)
 {
