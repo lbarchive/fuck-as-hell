5a22f4b1990f5524263698083b1980871c1440b2
Yu-Jie Lin <livibetter@gmail.com>
2012-03-21 20:35:45 +0800

    Initial fuck commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+ifeq ($(fuck),hell)
+fash: fash.o
+endif
+
+all:
+	@echo get the fuck off me
+
+apocalypse:
+	rm -f fash fash.o
+
+.PHONY: apocalypse
---
+++ fash.c
@@ -0,0 +1,14 @@
+int
+main (int argc, char **argv)
+{
+  // the fuck
+  char fuck[] = "Well, fuck everybody. Amen.";
+  // the hell
+  typedef char* hell;
+
+  // fuck as hell
+  puts((hell) fuck);
+
+  // fuck to the end
+  return fuck[sizeof fuck];
+}
